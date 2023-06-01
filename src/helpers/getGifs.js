25bf85b7e8eef113d1a872335b0109c8404fe92d
Oscar <chikenlitle99@hotmail.com>
2023-05-31 22:49:33 -0600

    Los gifs sin titulo obtienen uno por defecto

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -3,11 +3,21 @@ export const getGifs = async (category) => {
   const resp = await fetch(url);
   const { data } = await resp.json();
 
-  const gifs = data.map((img) => ({
-    id: img.id,
-    title: img.title,
-    url: img.images.original.url,
-  }));
+  const gifs = data.map((img) => {
+    if (img.title.trim().length < 1) {
+      return {
+        id: img.id,
+        title: `${category} GIF`,
+        url: img.images.original.url,
+      };
+    } else {
+      return {
+        id: img.id,
+        title: img.title,
+        url: img.images.original.url,
+      };
+    }
+  });
 
   return gifs;
 };
